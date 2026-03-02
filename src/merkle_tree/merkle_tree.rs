a03ef1471a2e3821ec8ce5ca6b05b89d9c364c7b
LYC386 <alex98605@gmail.com>
2026-03-02 17:18:05 +0800

    add merkle proof & change to BabyBear curve

--- src/merkle_tree/merkle_tree.rs
+++ src/merkle_tree/merkle_tree.rs
@@ -1,28 +1,21 @@
-use p3_bn254::Bn254;
-use p3_bn254::Poseidon2Bn254;
+use p3_baby_bear::{BabyBear, Poseidon2BabyBear};
 use p3_field::PrimeCharacteristicRing;
 use p3_symmetric::{PseudoCompressionFunction, TruncatedPermutation};
-use rand::rngs::ThreadRng;
 
 // WIDTH=3: state = [left, right, 0(capacity)] → permute → output state[0]
 // CHUNK=1: each input/output is a single Bn254 element
 // N=2: two inputs compressed to one output
-type Poseidon2Compress = TruncatedPermutation<Poseidon2Bn254<3>, 2, 1, 3>;
-
+type Poseidon2Compress = TruncatedPermutation<Poseidon2BabyBear<16>, 2, 1, 16>;
 #[derive(Debug, Clone)]
 pub struct MerkleTree {
-    pub root: Bn254,
+    pub root: BabyBear,
     pub depth: usize,
-    pub nodes: Vec<Vec<Bn254>>,
+    pub nodes: Vec<Vec<BabyBear>>,
 }
 
 impl MerkleTree {
-    pub fn new(depth: usize, leaves: Option<Vec<Bn254>>) -> Self {
-        let mut rng = ThreadRng::default();
-        let perm = Poseidon2Bn254::<3>::new_from_rng(8, 22, &mut rng);
-        let compress = Poseidon2Compress::new(perm);
-
-        let zero = Bn254::ZERO;
+    pub fn new(depth: usize, leaves: Option<Vec<BabyBear>>) -> Self {
+        let zero = BabyBear::ZERO;
         // Build each level bottom-up; start with the zero digest
         let mut current_level = match leaves {
             Some(leaves) => {
@@ -38,10 +31,10 @@ impl MerkleTree {
         };
         let mut nodes = vec![current_level.clone()];
 
-        for _ in 0..depth - 1 {
+        for _ in 0..depth {
             current_level = current_level
                 .chunks(2)
-                .map(|pair| compress.compress([[pair[0]], [pair[1]]])[0])
+                .map(|pair| poseidon2_two_to_one(pair[0], pair[1]))
                 .collect();
             nodes.push(current_level.clone());
         }
@@ -49,6 +42,43 @@ impl MerkleTree {
         let root = nodes.last().unwrap()[0];
         Self { root, depth, nodes }
     }
+
+    pub fn gen_merkle_proof(&self, index: usize) -> Vec<BabyBear> {
+        let mut current_index = index;
+        let mut proof_nodes = vec![];
+        for i in 0..self.depth {
+            if current_index & 1 == 0 {
+                proof_nodes.push(self.nodes[i][current_index + 1]);
+            } else {
+                proof_nodes.push(self.nodes[i][current_index - 1])
+            }
+            current_index = current_index >> 1;
+        }
+        proof_nodes
+    }
+
+    pub fn verify_merkle_proof(&self, proof: Vec<BabyBear>, index: usize) -> bool {
+        let mut current_index = index;
+        let mut result = self.nodes[0][index];
+        for node in proof {
+            if current_index & 1 == 0 {
+                result = poseidon2_two_to_one(result, node);
+            } else {
+                result = poseidon2_two_to_one(node, result);
+            }
+            current_index = current_index >> 1;
+        }
+        result == self.root
+    }
+}
+
+pub fn poseidon2_two_to_one(a: BabyBear, b: BabyBear) -> BabyBear {
+    // let mut rng = ThreadRng::default();
+    // let perm = Poseidon2BabyBear::<16>::new_from_rng(8, 22, &mut rng);
+    // let compress = Poseidon2Compress::new(perm);
+    let perm = p3_baby_bear::default_babybear_poseidon2_16();
+    let compress = Poseidon2Compress::new(perm);
+    compress.compress([[a], [b]])[0]
 }
 
 #[cfg(test)]
@@ -56,9 +86,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
-        let leaves: Vec<Bn254> = vec![Bn254::from_u32(1), Bn254::ZERO, Bn254::from_u32(2)];
+    fn test_proof() {
+        let leaves: Vec<BabyBear> = vec![
+            BabyBear::from_u32(1),
+            BabyBear::ZERO,
+            BabyBear::from_u32(2),
+            BabyBear::from_u32(46),
+            BabyBear::from_u32(78),
+        ];
         let mt = MerkleTree::new(3, Some(leaves));
-        println!("{:?}", mt);
+        let pf = mt.gen_merkle_proof(4);
+        println!("tree: {:?}", mt);
+        println!("proof: {:?}", pf);
+        println!("result: {}", mt.verify_merkle_proof(pf, 4))
     }
 }
